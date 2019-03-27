ea29cab05d5703fd98aade1cbec707c06f7988cc
sofu0185 <sofu0185@edu.easj.dk>
2019-03-27 12:27:43 +0100

    added logic in index.ts

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,16 @@
-import axios from '../../node_modules/axios/index'
+import axios, { AxiosResponse, AxiosError } from '../../node_modules/axios/index';
+import { ICar } from './ICar';
+
+let contentElement: HTMLDivElement = <HTMLDivElement>document.getElementById('content');
+
+function showAllCars(): void {
+    axios.get<ICar[]>('https://webapicar20190326034339.azurewebsites.net/api/cars')
+        .then(function(response: AxiosResponse<ICar[]>): void {
+            console.log(response.data)
+        })
+        .catch(function(error: AxiosError): void {
+            console.error(error)
+        });
+}
+
+showAllCars();
